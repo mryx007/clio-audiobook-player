7f80f8a24a4a185503133542926646d60abd57a1
Georg <C:\Users\Georg\AppData\Roaming\The Bat!>
2026-09-16 20:40:24 +0200

    fix: update issue and discussion links to mryx007/clio-audiobook-player

--- features/settings/src/main/kotlin/voice/features/settings/SettingsViewModel.kt
+++ features/settings/src/main/kotlin/voice/features/settings/SettingsViewModel.kt
@@ -222,15 +222,15 @@ class SettingsViewModel(
   }
 
   override fun getSupport() {
-    navigator.goTo(Destination.Website("https://github.com/PaulWoitaschek/Voice/discussions/categories/q-a"))
+    navigator.goTo(Destination.Website("https://github.com/mryx007/clio-audiobook-player/discussions"))
   }
 
   override fun suggestIdea() {
-    navigator.goTo(Destination.Website("https://github.com/PaulWoitaschek/Voice/discussions/categories/ideas"))
+    navigator.goTo(Destination.Website("https://github.com/mryx007/clio-audiobook-player/discussions"))
   }
 
   override fun openBugReport() {
-    val url = "https://github.com/PaulWoitaschek/Voice/issues/new".toUri()
+    val url = "https://github.com/mryx007/clio-audiobook-player/issues/new".toUri()
       .buildUpon()
       .appendQueryParameter("template", "bug.yml")
       .appendQueryParameter("version", appInfoProvider.versionName)
